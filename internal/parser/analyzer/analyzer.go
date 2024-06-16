f1865e7e4ddca44186a64c909cbb0614a2f770e8
ignacior <ignacioruedaboada@hotmail.com>
2024-06-16 18:08:06 -0500

    refactor: update error names

--- internal/parser/analyzer/analyzer.go
+++ internal/parser/analyzer/analyzer.go
@@ -11,8 +11,8 @@ import (
 )
 
 var (
-	ErrSectionExtractorParsingJSON         = errors.New("error parsing JSON value")
-	ErrSectionExtractorParsingBooleanValue = errors.New("error parsing boolean value")
+	ErrParsingMapValue     = errors.New("error parsing JSON value")
+	ErrParsingBooleanValue = errors.New("error parsing boolean value")
 )
 
 type Analyzer interface {
@@ -23,6 +23,37 @@ type analyzer struct{}
 
 func New() Analyzer {
 	return &analyzer{}
+}
+
+func isString(value string) bool {
+	return (strings.HasPrefix(value, `"`) && strings.HasSuffix(value, `"`)) ||
+		(strings.HasPrefix(value, "`") && strings.HasSuffix(value, "`"))
+}
+
+func isMap(value string) bool {
+	return strings.HasPrefix(value, "{") && strings.HasSuffix(value, "}")
+}
+
+func isBool(value string) bool {
+	return value == "true" || value == "false"
+}
+
+func isInt(value string) (int, bool) {
+	val, err := strconv.ParseInt(value, 10, 64)
+	if err == nil {
+		return int(val), true
+	}
+
+	return 0, false
+}
+
+func isFloat(value string) (float64, bool) {
+	val, err := strconv.ParseFloat(value, 64)
+	if err == nil {
+		return val, true
+	}
+
+	return 0, false
 }
 
 func (a *analyzer) Analyze(expressions types.SectionExpressions) (map[string]interface{}, error) {
@@ -38,32 +69,29 @@ func (a *analyzer) Analyze(expressions types.SectionExpressions) (map[string]int
 		key := strings.TrimSpace(parts[0])
 		value := strings.TrimSpace(parts[1])
 
-		if strings.HasPrefix(value, `"`) && strings.HasSuffix(value, `"`) {
+		if isString(value) {
 			// if value is a string
 			result[key] = strings.Trim(value, `"`)
-		} else if strings.HasPrefix(value, "`") && strings.HasSuffix(value, "`") {
-			// if value is a string
-			result[key] = strings.Trim(value, "`")
-		} else if strings.HasPrefix(value, "{") && strings.HasSuffix(value, "}") {
+		} else if isMap(value) {
 			// if value is a JSON object
 			var obj map[string]interface{}
 			if err := json.Unmarshal([]byte(value), &obj); err != nil {
-				return nil, ErrSectionExtractorParsingJSON
+				return nil, ErrParsingMapValue
 			}
 			result[key] = obj
-		} else if value == "true" || value == "false" {
+		} else if isBool(value) {
 			// if value is a boolean
 			b, err := strconv.ParseBool(value)
 			if err != nil {
-				return nil, ErrSectionExtractorParsingBooleanValue
+				return nil, ErrParsingBooleanValue
 			}
 			result[key] = b
-		} else if num, err := strconv.ParseInt(value, 10, 64); err == nil {
+		} else if num, ok := isInt(value); ok {
 			// if value is an integer
 			result[key] = int(num)
-		} else if num, err := strconv.ParseFloat(value, 64); err == nil {
+		} else if floatNum, isOk := isFloat(value); isOk {
 			// if value is a number
-			result[key] = num
+			result[key] = floatNum
 		} else {
 			// otherwise, value is a string
 			result[key] = value
