04cafbad310442db7d6727cc68600eae22452763
jibaru <ignacioruedaboada@hotmail.com>
2024-06-22 18:28:50 -0500

    feat: add map values validate methods

--- internal/types/section_types.go
+++ internal/types/section_types.go
@@ -13,3 +13,29 @@ type ReferenceToVariable struct {
 func NewReferenceToVariable(value string) ReferenceToVariable {
 	return ReferenceToVariable{Value: value}
 }
+
+// HasBasicTypesValues returns true if all values in the map are basic types (String, Int, Float, Bool)
+func (m Map) HasBasicTypesValues() bool {
+	for _, v := range m {
+		switch v.(type) {
+		case String, Bool, Int, Float:
+			continue
+		default:
+			return false
+		}
+	}
+	return true
+}
+
+// HasStringValues returns true if all values in the map are strings
+func (m Map) HasStringValues() bool {
+	for _, v := range m {
+		switch v.(type) {
+		case String:
+			continue
+		default:
+			return false
+		}
+	}
+	return true
+}
