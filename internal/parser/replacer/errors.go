8e0aa6ed4227f59fecdddaa26e6b40159b6ce1ba
ignacior <ignacioruedaboada@hotmail.com>
2024-07-01 10:56:18 -0500

    feat: update replacer errors

--- internal/parser/replacer/errors.go
+++ internal/parser/replacer/errors.go
@@ -5,10 +5,20 @@ type ReferenceToVariableNotFoundError struct {
 	referenceName string
 }
 
+type LetVariablesNotBasicTypesError struct{}
+
 func NewReferenceToVariableNotFoundError(key, referenceName string) error {
 	return ReferenceToVariableNotFoundError{key, referenceName}
 }
 
 func (e ReferenceToVariableNotFoundError) Error() string {
 	return "reference to variable for key " + e.key + " not found: " + e.referenceName
+}
+
+func NewInvalidLetVariablesError() error {
+	return LetVariablesNotBasicTypesError{}
+}
+
+func (e LetVariablesNotBasicTypesError) Error() string {
+	return "let variables must have basic types values"
 }
