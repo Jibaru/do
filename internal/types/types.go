3459d68d1112f03fc7f0cb677591fefa32eacea1
ignacior <ignacioruedaboada@hotmail.com>
2024-07-01 10:51:37 -0500

    feat: add sentences

--- internal/types/types.go
+++ internal/types/types.go
@@ -62,3 +62,78 @@ func (c CommandLineOutput) MarshalIndent() string {
 	value, _ := json.MarshalIndent(c, "", "   ")
 	return string(value)
 }
+
+// Sentence defines a key-value pair
+type Sentence struct {
+	Key   string
+	Value interface{}
+}
+
+// Sentences defines a ordered list of sentences
+type Sentences struct {
+	All         []Sentence
+	KeysWithIdx map[string]int
+}
+
+// NewSentences creates a new Sentences
+func NewSentences() *Sentences {
+	return &Sentences{
+		All:         make([]Sentence, 0),
+		KeysWithIdx: make(map[string]int),
+	}
+}
+
+// NewSentencesFromSlice creates a new Sentences from a slice of Sentence
+func NewSentencesFromSlice(sentences []Sentence) *Sentences {
+	sentencesMap := make(map[string]int)
+	for i, sentence := range sentences {
+		sentencesMap[sentence.Key] = i
+	}
+
+	return &Sentences{
+		All:         sentences,
+		KeysWithIdx: sentencesMap,
+	}
+}
+
+// Entries returns all sentences
+func (s *Sentences) Entries() []Sentence {
+	return s.All
+}
+
+// Get returns the value of a key
+func (s *Sentences) Get(key string) (interface{}, bool) {
+	idx, ok := s.KeysWithIdx[key]
+	if !ok {
+		return nil, false
+	}
+
+	return s.All[idx].Value, true
+}
+
+// Has returns true if the key exists
+func (s *Sentences) Has(key string) bool {
+	_, ok := s.KeysWithIdx[key]
+	return ok
+}
+
+// Set sets the value of a key
+func (s *Sentences) Set(key string, value interface{}) {
+	idx, ok := s.KeysWithIdx[key]
+	if ok {
+		s.All[idx].Value = value
+	} else {
+		s.All = append(s.All, Sentence{Key: key, Value: value})
+		s.KeysWithIdx[key] = len(s.All) - 1
+	}
+}
+
+// ToMap converts Sentences to a Map
+func (s *Sentences) ToMap() Map {
+	m := make(map[string]interface{})
+	for _, sentence := range s.All {
+		m[sentence.Key] = sentence.Value
+	}
+
+	return m
+}
